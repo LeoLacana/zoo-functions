7681303aeefa9c86093d50ba55de33c1111c5760
LeoLacana <wilson17414@gmail.com>
2021-05-12 21:20:42 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -31,7 +31,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((elementsEmployees) => elementsEmployees.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
